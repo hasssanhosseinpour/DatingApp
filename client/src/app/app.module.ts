0536345f769091cd5d29aebd9eb584a9f614ad36
Farideh Lamee Hassan Kiadeh <f_lamee2002@yahoo.com>
2023-08-09 12:11:28 -0700

    End of section 6 - 69-toast

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,13 +10,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +34,11 @@ import { RegisterComponent } from './register/register.component';
     FormsModule,
     //these below two lines have been added manually. Lesson 55
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass : 'toast-bottom-right'
+    }) // ToastrModule added by installing npm install ngx-toastr@15. 
+    //we need to install ngx-toast version 15 for Angular version 14.
   ],
   providers: [],
   bootstrap: [AppComponent]
